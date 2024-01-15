7db3fa8dee400908d555baff343191fa1b609fae
jing guo <mail@guojing.io>
2024-01-15 17:19:06 +0800

    multi monitors config, global git config, something else

--- private_dot_local/bin/executable_popup-calendar.sh
+++ private_dot_local/bin/executable_popup-calendar.sh
@@ -33,7 +33,7 @@ case "$1" in
 		: $((pos_y = BAR_HEIGHT + BORDER_SIZE))
 	fi
 
-	yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+	yad --calendar --mouse --undecorated --fixed --close-on-unfocus --no-buttons \
 		--width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
 		--title="yad-calendar" --show-weeks --borders="$BORDER_SIZE" >/dev/null &
 	;;
