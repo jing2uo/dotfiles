b2d46a096cb47ded940dbbc96344d7b1b43ccc02
jing guo <mail@guojing.io>
2025-03-03 14:01:37 +0800

    use swaync

--- private_dot_config/waybar/executable_reminder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-notify-send "站起来走走吧！" "坐太久了，活动一下！" -i sunflower
---
+++ private_dot_local/bin/executable_reminder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+notify-send "站起来走走吧！" "坐太久了，活动一下！" -i /usr/share/icons/Papirus/128x128/apps/sunflower.svg
