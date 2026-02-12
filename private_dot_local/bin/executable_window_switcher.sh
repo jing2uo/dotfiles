6c71777a5321924f98edd6f46f590c9d0378d126
Komh <mail@guojing.io>
2026-02-12 08:55:25 +0800

    use mise, systemd timer, zim module
    Signed-off-by: Komh <mail@guojing.io>

--- private_dot_local/bin/executable_window_switcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-windows=$(hyprctl clients | grep "title:" | awk -F'title: ' '{print $2}')
-selected=$(echo "$windows" | fuzzel --dmenu)
-if [ -n "$selected" ]; then
-  hyprctl dispatch focuswindow "title:$selected"
-fi
