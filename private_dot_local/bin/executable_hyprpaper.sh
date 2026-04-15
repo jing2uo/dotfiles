65e63eb48a35070473802c57e041747602a6841b
Komh <mail@guojing.io>
2026-04-15 22:23:58 +0800

    update, some about thinkpad l14

--- private_dot_local/bin/executable_hyprpaper.sh
+++ private_dot_local/bin/executable_hyprpaper.sh
@@ -2,6 +2,5 @@
 
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
 WALLPAPER=$(fdfind . "$WALLPAPER_DIR" | shuf -n 1)
-MONITOR=$(hyprctl monitors -j | jq -r '.[0].name')
 
-hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER,cover"
+hyprctl hyprpaper wallpaper ",$WALLPAPER,cover"
